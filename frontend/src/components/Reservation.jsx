6e6b887f3d4add542f837c7dd7ed38f69efe56fc
Avinash <parugondavinash14@gmail.com>
2024-03-23 02:24:08 +0530

    All

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://wild-jade-badger-robe.cyclic.app/api/v1/reservation/send",
         { firstName, lastName, email, date, time, phone },
         {
           headers: {
